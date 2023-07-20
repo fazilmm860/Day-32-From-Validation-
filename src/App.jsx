9bdcf6b9dd99a0533f0eaae719ea411ee52733eb
Mohamed Fazil M <fazilmm860@gmail.com>
2023-07-20 15:45:49 +0530

    design of forms done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 import './App.css';
+import BasicForm from './components/BasicForm';
+import AdvancedForm from './components/AdvancedForm';
 
 function App() {
+  const [view,setView]=useState("basic")
   return (
     <div className="App">
      <nav>
-      <h3>
+      <h3 
+      onClick={()=>setView("basic")}
+      style={{color: view ==='basic' ? "#fff" : ""}}
+      >
         Basic
       </h3>
-      <h3>
+      <h3
+      onClick={()=>setView("advanced")}
+      style={{color: view ==='advanced' ? "#fff" : ""}}
+      >
         Advanced
       </h3>
      </nav>
+     {view === "basic" ? <BasicForm/> : <AdvancedForm />}
     </div>
   );
 }
